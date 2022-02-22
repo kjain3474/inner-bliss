10b5b6e9471ed7e582ba21d6b36683c40762a7d0
Kapil Jain <kjain3474@gmail.com>
2022-02-22 23:19:48 +0530

    image updates and logo changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import logo from './innerbliss_main_image.jpeg';
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SocialIcon bgColor="#831C78" network="instagram" url="https://www.instagram.com/innerbliss.co" />
       </header>
     </div>
   );
